a0177169c6ce6693a47b79a1dd722d0faa79a90e
deathblade666 <halo2ofdeath@gmail.com>
2024-06-30 19:35:22 -0400

    Add Base option menu

--- lib/pages/split_edit.dart
+++ lib/pages/split_edit.dart
@@ -1,7 +1,19 @@
 import 'dart:core';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:mdeditor/pages/preview.dart';
 import 'package:mdeditor/pages/textfield.dart';
+import 'package:mdeditor/pages/functions/files.dart';
+
+
+ enum Menu {
+  save, 
+  open,
+  fileInfo,
+  switchTheme,
+  switchView,
+  enableWorkCount
+  }
 
 class Editor extends StatefulWidget {
   const Editor({super.key});
@@ -37,6 +49,43 @@ class editorState extends State<Editor> {
           ),
           Expanded(
             child: mdtextfield(ontextchanged: mdText,),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              PopupMenuButton(
+                padding: const EdgeInsets.only(left: 5),
+                elevation: 0,
+                color: Theme.of(context).primaryColor,
+                itemBuilder: (BuildContext context) => <PopupMenuEntry<Menu>>[
+                  const PopupMenuItem<Menu>(
+                    value: Menu.save,
+                    //nTap: save,
+                    child: Text("Save"),
+                  ),
+                  const PopupMenuItem<Menu>(
+                    value: Menu.open,
+                    //onTap: openFile,
+                    child: Text("Open"),
+                  ),
+                  const PopupMenuItem<Menu>(
+                    value: Menu.fileInfo,
+                    //onTap: openFile,
+                    child: Text("File Info"),
+                  ),
+                  const PopupMenuItem<Menu>(
+                    value: Menu.switchView,
+                    //onTap: openFile,
+                    child: Text("Switch Mode"),
+                  ),
+                  const PopupMenuItem<Menu>(
+                    value: Menu.switchTheme,
+                    //onTap: openFile,
+                    child: Text("Change Theme"),
+                  ),   
+                ]
+              )
+            ]
           ),
         ]
       ),
