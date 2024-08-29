1d61438dfe98c844f8a6a79941e25b204234a2b7
deathblade666 <halo2ofdeath@gmail.com>
2024-08-29 16:59:58 -0400

    fix deps

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,13 +9,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mdeditor/main.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await tester.pumpWidget(  MyApp(prefs));
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
